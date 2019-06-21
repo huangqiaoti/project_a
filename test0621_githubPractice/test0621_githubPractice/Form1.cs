24885cebde11474eedc316569c84697d86a07192
Huang <hquue04@gmail.com>
2019-06-21 09:59:31 +0800

    project created, button2 added.

--- test0621_githubPractice/test0621_githubPractice/Form1.cs
+++ test0621_githubPractice/test0621_githubPractice/Form1.cs
@@ -16,5 +16,15 @@ namespace test0621_githubPractice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "OK 1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "OK 2";
+        }
     }
 }
